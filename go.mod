8b18149fa717000a1931ca0ed3832f5b1e75a56f
Jens Zeilund <jzs@sketchground.dk>
2026-03-20 13:41:29 +0100

    chore: add go.mod to project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jzs/sqlm
+
+go 1.26.1
--- sqlm_test.go
+++ sqlm_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/sketchground/sqlm"
+	"github.com/jzs/sqlm"
 )
 
 // DBShopgunAuth struct to represent an auth token
